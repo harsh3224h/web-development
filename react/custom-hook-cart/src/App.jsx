d6e4ec72e48e1f29d21557e0fa1f4da546ba6c98
Harsh Kumar <harshdhiman7044@gmail.com>
2026-05-10 12:59:16 +0530

    Done with custom hook product cart project

--- react/custom-hook-cart/src/App.jsx
+++ react/custom-hook-cart/src/App.jsx
@@ -1,12 +1,38 @@
 import "./App.css";
+import { useCart } from "./hooks/useCart";
+import ProductCard from "./components/ProductCard";
+import { products } from "./data/products";
+import Cart from "./components/Cart";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const { cart, addToCart, removeFromCart, updateQuantity, total } = useCart();
   return (
     <>
-      <div></div>
-      <h1>Custom hook class</h1>
+      <div className="app">
+        <header>
+          <h1>Shopping Cart</h1>
+        </header>
+
+        <main>
+          <section className="products">
+            {products.map((product) => (
+              <ProductCard
+                key={product.id}
+                product={product}
+                onAddToCart={addToCart}
+              />
+            ))}
+          </section>
+          <section>
+            <Cart
+              cart={cart}
+              total={total}
+              onUpdateQuantity={updateQuantity}
+              onRemove={removeFromCart}
+            />
+          </section>
+        </main>
+      </div>
     </>
   );
 }
