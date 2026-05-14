9a0b11978d579c03ffab608d62dc956148bb39a9
Harsh Kumar <harshdhiman7044@gmail.com>
2026-05-14 11:33:53 +0530

    Created path listening features in the http server

--- NodeJS/http-server/index.js
+++ NodeJS/http-server/index.js
@@ -1,11 +1,30 @@
-const http = require("http");
+const http = require("node:http");
 
-const server = http.createServer(function (req, res) {
-  console.log("I got a incoming request");
-  res.writeHead(200);
-  res.end("Request recieved successfully");
+const server = http.createServer((req, res) => {
+  // console.log(`Incoming request at ${Date.now()}`);
+  // console.log(req.headers);
+  // console.log(req.method);
+  // res.end(`You can accept in ${req.headers["accept-language"]}`);
+
+  console.log(req.url);
+  switch (req.url) {
+    case "/":
+      res.writeHead(200);
+      return res.end("HomePage");
+
+    case "/contact":
+      res.writeHead(200);
+      return res.end("Contact Page");
+
+    case "/about":
+      res.writeHead(200);
+      return res.end("About Page");
+    default:
+      res.writeHead(404);
+      return res.end("You're lost");
+  }
 });
 
-server.listen(8000, function () {
-  console.log("HTTP server is running on port 8000");
+server.listen(8000, () => {
+  console.log("server is running on port 8000");
 });
