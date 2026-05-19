413269cb58f2281762b69a682a6f25c251e3da2c
Harsh Kumar <harshdhiman7044@gmail.com>
2026-05-19 10:35:46 +0530

    feat: index based searching

--- NodeJS/express/bookstore-api/controllers/book.controller.js
+++ NodeJS/express/bookstore-api/controllers/book.controller.js
@@ -1,16 +1,18 @@
 // const db = require("../db/index.js");
 const db = require("../db/index.js");
 const { eq, ilike } = require("drizzle-orm");
+const { sql } = require("drizzle-orm");
 const { booksTable, authorsTable } = require("../models/index.js");
 
 exports.getAllBooks = async function (req, res) {
   const search = req.query.search;
-  // console.log(search);
   if (search) {
     const books = await db
       .select()
       .from(booksTable)
-      .where(ilike(booksTable.title, `%${search}%`));
+      .where(
+        sql`to_tsvector('english', ${booksTable.title}) @@ to_tsquery('english', ${search})`,
+      );
     if (books) return res.status(201).json(books);
   }
   const books = await db.select().from(booksTable);
--- NodeJS/express/bookstore-api/models/book.model.js
+++ NodeJS/express/bookstore-api/models/book.model.js
@@ -1,13 +1,23 @@
-const { pgTable, uuid, varchar, text } = require("drizzle-orm/pg-core");
+const { pgTable, uuid, varchar, text, index } = require("drizzle-orm/pg-core");
+const { sql } = require("drizzle-orm");
 const authorsTable = require("./author.model.js");
 
-const booksTable = pgTable("books", {
-  id: uuid().primaryKey().defaultRandom(),
-  title: varchar({ length: 100 }).notNull(),
-  description: text(),
-  authorId: uuid()
-    .references(() => authorsTable.id)
-    .notNull(),
-});
+const booksTable = pgTable(
+  "books",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    title: varchar({ length: 100 }).notNull(),
+    description: text(),
+    authorId: uuid()
+      .references(() => authorsTable.id)
+      .notNull(),
+  },
+  (table) => ({
+    SearchIndexOnTitle: index("title_index").using(
+      "gin",
+      sql`to_tsvector('english', ${table.title})`,
+    ),
+  }),
+);
 
 module.exports = booksTable;
