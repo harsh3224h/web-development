479b0cee756c60f37d747c5ac87cb48e5aebe1dc
Harsh Kumar <harshdhiman7044@gmail.com>
2026-06-24 09:38:52 +0530

    Initial typescript project setup

---
+++ TypeScript/basic/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello ${person}, let's learn TypeScript!`;
+}
+console.log(greet("Harsh"));
+//# sourceMappingURL=index.js.map
---
+++ TypeScript/basic/src/index.ts
@@ -0,0 +1,5 @@
+function greet(person: string): string {
+  return `Hello ${person}, let's learn TypeScript!`
+}
+
+console.log(greet("Harsh"));
