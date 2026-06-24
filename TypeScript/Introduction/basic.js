8840d4cde0384bb6a3f0c7b3f1dc86cd83c4ddb9
Harsh Kumar <harshdhiman7044@gmail.com>
2026-06-24 08:15:18 +0530

    TypeScript journey started

---
+++ TypeScript/Introduction/basic.js
@@ -0,0 +1,6 @@
+function greet(name) {
+  return `Hello, ${name}.`;
+}
+
+console.log(greet("Harsh"));
+console.log(greet(42)); // this is not the expected parameter
---
+++ TypeScript/Introduction/hello.ts
@@ -0,0 +1,7 @@
+function greet(name: string): string {
+  return `Hello, ${name}.`;
+}
+
+console.log(greet("Harsh"));
+
+// console.log(greet(42));
