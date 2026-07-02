8ad26955bd0f6c65ed73a60f1fab8fc0fec69ff6
Harsh Kumar <harshdhiman7044@gmail.com>
2026-07-02 17:20:37 +0530

    add typeScript object

---
+++ TypeScript/objects/src/index.ts
@@ -0,0 +1,20 @@
+const chai = {
+  name: "Masala Chai",
+  price: "20rs",
+  sugerBlocks: 2,
+  isHot: true,
+}
+
+let Chai1: {
+  name:string,
+  price: string,
+  sugerBlocks: number,
+  isHot: boolean
+}
+
+type Chai2 = {
+  name: string,
+  price: string,
+  sugerBlocks: number,
+  isHot: boolean
+}
